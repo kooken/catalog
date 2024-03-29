0bba077aa80fe8cf7253420bdf7fb2e87fe22967
mariasazhina <msazhina23@gmail.com>
2024-03-29 13:53:50 +0200

    web settings

---
+++ main/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(f"{name} {email}: {message}")
+    return render(request, 'main/index.html')
+
+def contacts(request):
+    return render(request, 'main/contacts.html')
