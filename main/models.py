7e67bbbc9f91b3a4e359ebd0d04080779028d085
mariasazhina <msazhina23@gmail.com>
2024-04-07 16:15:09 +0300

    added models & migrations

--- main/models.py
+++ main/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+NULLABLE = {'null': True,
+            'blank': True}
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=100, verbose_name='наименование')
+    category_description = models.CharField(max_length=200, verbose_name='описание')
+
+    def __str__(self):
+        return f'{self.category_name} {self.category_description}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+        ordering = ('category_name',)
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=100, verbose_name='наименование')
+    product_description = models.CharField(max_length=200, verbose_name='описание')
+    product_image = models.ImageField(upload_to='products/', verbose_name='изображение', **NULLABLE)
+    product_category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    product_price = models.IntegerField(verbose_name='цена за покупку')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.product_name} {self.product_description}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+        ordering = ('product_name',)
+
