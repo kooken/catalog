aec18def3a65ac9de9c0eb7f1493f30aa74b97e8
mariasazhina <msazhina23@gmail.com>
2024-05-05 18:33:34 +0300

    add new model version

--- main/models.py
+++ main/models.py
@@ -17,6 +17,7 @@ class Category(models.Model):
         verbose_name_plural = 'категории'
         ordering = ('category_name',)
 
+
 class Product(models.Model):
     product_name = models.CharField(max_length=100, verbose_name='наименование')
     product_description = models.CharField(max_length=200, verbose_name='описание')
@@ -33,4 +34,23 @@ class Product(models.Model):
         verbose_name = 'продукт'
         verbose_name_plural = 'продукты'
         ordering = ('product_name',)
+
+
+
+class Version(models.Model):
+    # product = models.CharField(max_length=100, verbose_name='продукт')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='version',
+                                verbose_name='наименование')
+    version_no = models.IntegerField(verbose_name='номер версии')
+    version_name = models.CharField(max_length=100, verbose_name='название версии')
+    active_version = models.BooleanField(default=True, verbose_name='признак текущей версии')
+
+    def __str__(self):
+        return f'{self.product} {self.version_name}'
+
+    class Meta:
+        verbose_name = 'версия'
+        verbose_name_plural = 'версии'
+
+
 
