bc5455a3ced0592500caa3b258a3c378b78791ff
mariasazhina <msazhina23@gmail.com>
2024-05-05 16:13:34 +0300

    can add new product

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from main.apps import MainConfig
-from main.views import ProductListView, ContactsView, ProductDetailView
+from main.views import ProductListView, ContactsView, ProductDetailView, ProductCreateView
 
 app_name = MainConfig.name
 
@@ -9,4 +9,6 @@ urlpatterns= [
     path('', ProductListView.as_view(), name='index'),
     path('products/<int:pk>', ProductDetailView.as_view(), name='products'),
     path('contacts/', ContactsView.as_view(), name='contacts'),
+    path('create/', ProductCreateView.as_view(), name='create'),
+
 ]
